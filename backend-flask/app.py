3260b7d265e4e3bad977545b3ef38d1f3d99bbd6
krunalijain <krunalijain2001@gmail.com>
2023-03-03 07:08:52 +0000

    add rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -146,7 +146,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run(logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
