c83ec7524a29bbb588a77a21a69e2530bef5a818
krunalijain <krunalijain2001@gmail.com>
2023-03-07 05:09:34 +0000

    backend for auth

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -147,6 +147,10 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  print('AUTH HEADER ----')
+  print(
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run()
   return data, 200
 
