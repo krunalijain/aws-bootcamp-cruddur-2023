884517e8d889d1d8e93bd76f4c39042504cec3f1
krunalijain <krunalijain2001@gmail.com>
2023-03-09 06:21:47 +0000

    getting type error

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -24,9 +24,9 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
-      // headers: {
-      //  Authorization: `Bearer ${localStorage.getItem("access_token")}`
-      // },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
@@ -57,7 +57,8 @@ export default function HomeFeedPage() {
         })
     })
     .catch((err) => console.log(err));
-  }; 
+  };
+  
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
