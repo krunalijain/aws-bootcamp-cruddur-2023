c887f74d2600b4f2bcab7db9f00f98f2d46a7d72
krunalijain <krunalijain2001@gmail.com>
2023-03-14 10:15:46 +0000

    commit first query code but still shows error

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
 
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 #tracer = trace.get_tracer("home.activities")
 
@@ -14,18 +14,16 @@ class HomeActivities:
 #    now = datetime.now(timezone.utc).astimezone()
 #    span.set_attribute("app.now", now.isoformat())
     
-    sql = """
+    sql = query_wrap_array("""
     SELECT * FROM activities 
-    """
+    """)
+    print()
     with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
-    print("===------------------")
-    print("===------------------")
-    print(json)
+          json = cur.fetchone()
     return json[0]
     return results
     
