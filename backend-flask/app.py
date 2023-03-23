7aff016ba62e0802933cc72d5cdb58e73bc877b7
krunalijain <krunalijain2001@gmail.com>
2023-03-23 09:10:41 +0000

    back to original script of user handle

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -197,7 +197,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'krunalijain'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
