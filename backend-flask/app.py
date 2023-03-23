23100575b08879562543a981cf6a52d2b84c455e
krunalijain <krunalijain2001@gmail.com>
2023-03-23 08:33:41 +0000

    hardcoded user handle under api/activities route

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -197,7 +197,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'krunalijain'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
