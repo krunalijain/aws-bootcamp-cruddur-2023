2996849dc131acac38fa45207a25efacdb5f4515
krunalijain <krunalijain2001@gmail.com>
2023-03-25 19:17:30 +0000

    backend not working!!

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -4,21 +4,22 @@ from lib.ddb import Ddb
 from lib.db import db
 
 class MessageGroups:
- def run(cognito_user_id):
+  def run(cognito_user_id):
     model = {
       'errors': None,
       'data': None
     }
-sql = db.template('users','uuid_from_cognito_user_id')
-my_user_uuid = db.query_value(sql,{
-    'cognito_user_id': cognito_user_id
+
+    sql = db.template('users','uuid_from_cognito_user_id')
+    my_user_uuid = db.query_value(sql,{
+      'cognito_user_id': cognito_user_id
     })
 
-print(f"UUID: {my_user_uuid}")
+    print(f"UUID: {my_user_uuid}")
 
-ddb = Ddb.client()
-data = Ddb.list_message_groups(ddb, my_user_uuid)
-print("list_message_groups:",data)
+    ddb = Ddb.client()
+    data = Ddb.list_message_groups(ddb, my_user_uuid)
+    print("list_message_groups:",data)
 
-model['data'] = data
-return model
+    model['data'] = data
+    return model
