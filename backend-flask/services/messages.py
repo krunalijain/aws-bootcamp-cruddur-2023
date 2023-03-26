132d81fbe525ea3fb453ec9556a13a8d3323c6ea
krunalijain <krunalijain2001@gmail.com>
2023-03-26 06:26:46 +0000

    fixed syntax error

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -3,22 +3,23 @@ from lib.ddb import Ddb
 from lib.db import db
 
 class Messages:
- def run(message_group_uuid,cognito_user_id):
+  def run(message_group_uuid,cognito_user_id):
     model = {
       'errors': None,
       'data': None
     }
- sql = db.template('users','uuid_from_cognito_user_id')
- my_user_uuid = db.query_value(sql,{
+
+    sql = db.template('users','uuid_from_cognito_user_id')
+    my_user_uuid = db.query_value(sql,{
       'cognito_user_id': cognito_user_id
     })
 
- print(f"UUID: {my_user_uuid}")
+    print(f"UUID: {my_user_uuid}")
 
- ddb = Ddb.client()
- data = Ddb.list_messages(ddb, message_group_uuid)
- print("list_messages")
- print(data)
- model['data'] = data
- return model
+    ddb = Ddb.client()
+    data = Ddb.list_messages(ddb, message_group_uuid)
+    print("list_messages")
+    print(data)
 
+    model['data'] = data
+    return model
