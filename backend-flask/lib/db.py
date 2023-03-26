c283110fae6acf51b28174d641468cfe468a690e
krunalijain <krunalijain2001@gmail.com>
2023-03-26 10:28:13 +0000

    faacing botocore error

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -3,20 +3,26 @@ import os
 import re
 import sys
 from flask import current_app as app
+
 class Db:
   def __init__(self):
     self.init_pool()
+
   def template(self,*args):
     pathing = list((app.root_path,'db','sql',) + args)
     pathing[-1] = pathing[-1] + ".sql"
+
     template_path = os.path.join(*pathing)
+    
     green = '\033[92m'
     no_color = '\033[0m'
     print("\n")
     print(f'{green} Load SQL Template: {template_path} {no_color}')
+    
     with open(template_path, 'r') as f:
       template_content = f.read()
     return template_content
+  
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
@@ -28,6 +34,7 @@ class Db:
     print(f'{blue} SQL Params:{no_color}')
     for key, value in params.items():
       print(key, ":", value)
+  
   def print_sql(self,title,sql,params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
@@ -38,6 +45,7 @@ class Db:
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
+    
     try:
       with self.pool.connection() as conn:
         cur =  conn.cursor()
@@ -52,6 +60,7 @@ class Db:
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql,params)
+    
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
@@ -60,9 +69,11 @@ class Db:
         return json[0]
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
+    
     self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
+    
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(wrapped_sql,params)
@@ -95,12 +106,16 @@ class Db:
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
+   
     # get the line number when exception occured
     line_num = traceback.tb_lineno
+    
     # print the connect() error
     print ("\npsycopg ERROR:", err, "on line number:", line_num)
     print ("psycopg traceback:", traceback, "-- type:", err_type)
+   
     # print the pgcode and pgerror exceptions
     print ("pgerror:", err.pgerror)
     print ("pgcode:", err.pgcode, "\n")
+    
 db = Db()
