1e1abaafb43aefba8851a6d23516b36ad91e8354
krunalijain <krunalijain2001@gmail.com>
2023-03-27 10:28:43 +0000

    hardcoded username

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Krunali Jain','krunalijain2001@gmail.com' , 'krunalijain' ,'1091d57b-1eb1-427d-9a1d-5b699d41075a'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'krunalijain' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
