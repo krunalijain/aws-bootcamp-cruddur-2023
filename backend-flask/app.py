72981e04567b04e8a69f5a20351423790891c821
krunalijain <krunalijain2001@gmail.com>
2023-04-01 18:22:44 +0000

    Set the repos for python andd flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -105,7 +105,7 @@ def init_rollbar():
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
-    @app.route('/api/health-check')
+@app.route('/api/health-check')
 def health_check():
   return {'success': True}, 200
 
