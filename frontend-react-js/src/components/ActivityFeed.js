9869ee0bb0064f5863450057ce7cd303679bae4b
krunalijain <krunalijain2001@gmail.com>
2023-04-25 18:58:55 +0000

    backend not working

--- frontend-react-js/src/components/ActivityFeed.js
+++ frontend-react-js/src/components/ActivityFeed.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 export default function ActivityFeed(props) {
   return (
     <div className='activity_feed_collection'>
-    {props.activities.map(activity => {
+    {props.activities && Array.isArray(props.activities) && props.activities.map(activity => {
     return  <ActivityItem setReplyActivity={props.setReplyActivity} setPopped={props.setPopped} key={activity.uuid} activity={activity} />
     })}
   </div>
