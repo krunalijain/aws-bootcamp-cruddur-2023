d947caa7fefce0cd1d3638d8132823b397771558
krunalijain <krunalijain2001@gmail.com>
2023-04-25 18:01:01 +0000

    fixed undefined setUser

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -14,6 +14,7 @@ export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [profile, setProfile] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
+  const [user, setUser] = React.useState(null);
   const [poppedProfile, setPoppedProfile] = React.useState([]);
   const dataFetchedRef = React.useRef(false);
 
