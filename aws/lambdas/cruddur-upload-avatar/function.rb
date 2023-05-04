47fbdd29c79f67b6c4b43c06dad64b5418b8054a
krunalijain <krunalijain2001@gmail.com>
2023-05-04 15:35:30 +0000

    undefined presignedurl error and CORS

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,20 +1,22 @@
 require 'aws-sdk-s3'
 require 'json'
-
 def handler(event:, context:)
   puts event
   s3 = Aws::S3::Resource.new
   bucket_name = ENV["UPLOADS_BUCKET_NAME"]
   object_key = 'mock.jpg'
-
   obj = s3.bucket(bucket_name).object(object_key)
   url = obj.presigned_url(:put, expires_in: 60 * 5)
   url # this is the data that will be returned
   body = {url: url}.to_json
-  { statusCode: 200, body: body }
-end
+  { 
+    headers: {
+      "Access-Control-Allow-Headers": "*, Authorization",
+      "Access-Control-Allow-Origin": "https://3000-krunalijain-awsbootcamp-i8adqr9w0l3.ws-us96b.gitpod.io",
+      "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+    },
+    statusCode: 200, 
+    body: body 
 
-puts handler(
-  event: {},
-  context: {}
-)
+  }
+end
