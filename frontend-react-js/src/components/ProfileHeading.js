beaef235c6a5087c0efbf5ffcbb4373820c4a22b
krunalijain <krunalijain2001@gmail.com>
2023-05-08 09:47:18 +0000

    render avatar

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,6 +2,7 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ProfileAvatar from 'components/ProfileAvatar'
 
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.iamdevopsgeek.cloud/banners/banner.jpg")';
@@ -15,9 +16,7 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.iamdevopsgeek.cloud/avatars/processed/data.jpg"></img>
-      </div>
+    <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -2,6 +2,7 @@ import './ProfileInfo.css';
 import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 import ReactDOM from 'react-dom';
+import ProfileAvatar from 'components/ProfileAvatar'
 
 // [TODO] Authenication
 import { Auth } from 'aws-amplify';
@@ -37,7 +38,7 @@ export default function ProfileInfo(props) {
         <button onClick={signOut}>Sign Out</button> 
       </div>
       <div className="profile-info" onClick={click_pop}>
-        <div className="profile-avatar"></div>
+      <ProfileAvatar id={props.user.cognito_user_uuid} />
         <div className="profile-desc">
           <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
           <div className="profile-username">@{props.user.handle || "handle"}</div>
