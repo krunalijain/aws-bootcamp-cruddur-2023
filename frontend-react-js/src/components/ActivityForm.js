2e84e27eb530f74be1ac9ba86ddae90d986f70db
krunalijain <krunalijain2001@gmail.com>
2023-06-13 14:59:44 +0000

    added authorization for crud post

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,6 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { checkAuth, getAccessToken } from '../lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -17,11 +18,15 @@ export default function ActivityForm(props) {
   const onsubmit = async (event) => {
     event.preventDefault();
     try {
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      console.log('activityform', access_token)
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
       console.log('onsubmit payload', message)
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
