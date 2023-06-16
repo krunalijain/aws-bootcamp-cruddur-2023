430686c5e5eefddcf4a35c88826aad20c35592af
krunalijain <krunalijain2001@gmail.com>
2023-06-16 15:04:55 +0000

    Replies fixed!! Still WIP

--- backend-flask/services/create_reply.py
+++ backend-flask/services/create_reply.py
@@ -1,23 +1,22 @@
 from datetime import datetime, timedelta, timezone
+
 from lib.db import db
+
 class CreateReply:
-   def run(message, cognito_user_id, activity_uuid):
+  def run(message, cognito_user_id, activity_uuid):
     model = {
       'errors': None,
       'data': None
     }
-
     if cognito_user_id == None or len(cognito_user_id) < 1:
       model['errors'] = ['cognito_user_id_blank']
 
     if activity_uuid == None or len(activity_uuid) < 1:
       model['errors'] = ['activity_uuid_blank']
-
     if message == None or len(message) < 1:
       model['errors'] = ['message_blank'] 
     elif len(message) > 1024:
       model['errors'] = ['message_exceed_max_chars'] 
-
     if model['errors']:
       # return what we provided
       model['data'] = {
@@ -31,7 +30,7 @@ class CreateReply:
       model['data'] = object_json
     return model
 
-def create_reply(cognito_user_id, activity_uuid, message):
+  def create_reply(cognito_user_id, activity_uuid, message):
     sql = db.template('activities','reply')
     uuid = db.query_commit(sql,{
       'cognito_user_id': cognito_user_id,
@@ -39,7 +38,7 @@ def create_reply(cognito_user_id, activity_uuid, message):
       'message': message,
     })
     return uuid
-def query_object_activity(uuid):
+  def query_object_activity(uuid):
     sql = db.template('activities','object')
     return db.query_object_json(sql,{
       'uuid': uuid
